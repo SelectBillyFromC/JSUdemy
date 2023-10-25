ed1324f0c06857dca05c37c8bdd00e1db293ea4a
SelectBillyFromC <61947286+SelectBillyFromC@users.noreply.github.com>
2023-10-25 01:06:09 -0400

    [#1] complete questions for assignment 2 #1

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,26 @@
 const task3Element = document.getElementById('task-3');
+
+// Regular Function
+//
+// function displayAlert() {
+//     alert("Hello!")
+// }
+
+// Anonymous Function use
+const anonDisplayAlert = () => {
+    alert("Hello, (Anonymous)")
+}
+
+function displayAlertWithName(name) {
+    alert(`Hello, ${name}!`)
+}
+task3Element.addEventListener('click', anonDisplayAlert)
+displayAlertWithName("Phil")
+
+const concatName = (str1, str2, str3) => {
+    let concatText = `${str1}, ${str2}, and ${str3}`;
+    return concatText;    
+}
+
+displayAlertWithName(concatName('Chaewon', 'Karina', 'Winter'))
+
